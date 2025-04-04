80f79003d3dd360505b4d351bfffa1993b6e310c
alirezadoostdar <alireza.doostdar@gmail.com>
2025-04-04 18:15:50 +0200

    Add Movie view model

--- src/Persentation/Movie.WPF/ViewModels/MovieViewModel.cs
+++ src/Persentation/Movie.WPF/ViewModels/MovieViewModel.cs
@@ -1,12 +1,133 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movie.Application.Dtos;
+using Movie.Application.Services;
+using Movie.WPF.Commands;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Movie.WPF.ViewModels;
 
-public class MovieViewModel
+public class MovieViewModel : BaseViewModel
 {
-    
+    private readonly MovieService movieService;
+
+    public MovieViewModel()
+    {
+        this.movieService = App.AppHost.Services.GetRequiredService<MovieService>();
+        Movies = new ObservableCollection<MovieDto>(movieService.GetAll());
+    }
+    #region Fields
+    private ObservableCollection<MovieDto> movies;
+
+    public ObservableCollection<MovieDto> Movies
+    {
+        get { return movies; }
+        set
+        {
+            movies = value;
+            NotifyPropertyChanged(nameof(Movies));
+        }
+    }
+
+    private MovieDto selectedIndex;
+
+    public MovieDto SelectedIndex
+    {
+        get { return selectedIndex; }
+        set { selectedIndex = value; NotifyPropertyChanged(); }
+    }
+
+    private int id;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; NotifyPropertyChanged(); }
+    }
+
+    private string title;
+
+    public string Title
+    {
+        get { return title; }
+        set { title = value; NotifyPropertyChanged(); }
+    }
+
+    private decimal imdb;
+
+    public decimal Imdb
+    {
+        get { return imdb; }
+        set { imdb = value; NotifyPropertyChanged(); }
+    }
+
+    private int directorId;
+
+    public int DirectorId
+    {
+        get { return directorId; }
+        set { directorId = value; NotifyPropertyChanged(); }
+    }
+
+    private string description;
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; NotifyPropertyChanged(); }
+    }
+
+    private string year;
+
+    public string Year
+    {
+        get { return year; }
+        set { year = value; NotifyPropertyChanged(); }
+    }
+
+    private string poster;
+
+    public string Poster
+    {
+        get { return poster; }
+        set { poster = value; NotifyPropertyChanged(); }
+    }
+
+    private string cast;
+            
+    public string Cast
+    {
+        get { return cast; }
+        set { cast = value; NotifyPropertyChanged(); }
+    }
+
+
+    #endregion
+
+    #region Command
+    public RelayCommand AddCommand => new RelayCommand(execute => Add(), canExecute => IsValid());
+
+    private void Add()
+    {
+        try
+        {
+            movieService.Add(SelectedIndex);
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    private bool IsValid()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
 }
--- src/core/Movie.Application/Services/MovieService.cs
+++ src/core/Movie.Application/Services/MovieService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Movie.Application.Dtos;
+using Movie.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,44 @@ namespace Movie.Application.Services;
 
 public class MovieService
 {
+    private readonly IFilmRepository repository;
 
+    public MovieService(IFilmRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public IEnumerable<MovieDto> GetAll()
+    {
+        return repository.GetAll().Select(x => new MovieDto()
+        {
+            Id = x.Id,
+            Title = x.Title,
+            DirectorId = x.DirectorId,
+            Description = x.Description,
+            CreateDate = x.CreateDate,
+            Tagline = x.Tagline,
+            ImbdRate = x.ImbdRate,
+            Poster = x.Poster,
+            Cast = x.Cast,
+            Year = x.Year,  
+        });
+    }
+
+    public void Add(MovieDto movie)
+    {
+        repository.Add(new Domain.Entities.Film
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            DirectorId = movie.DirectorId,
+            Description = movie.Description,
+            CreateDate = movie.CreateDate,
+            Tagline = movie.Tagline,
+            ImbdRate = movie.ImbdRate,
+            Poster = movie.Poster,
+            Cast = movie.Cast,
+            Year = movie.Year,
+        });
+    }
 }
